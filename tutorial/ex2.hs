343799ef76238ec5106ccd6dc29917e2826ac37c
Richo Healey <richo@psych0tik.net>
2013-06-05 19:15:36 +1000

    Implement parsing of standard type
    no output of matching types, no show implementation

--- tutorial/ex2.hs
+++ tutorial/ex2.hs
@@ -1,6 +1,7 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
 symbol :: Parser Char
 symbol = oneOf "!#$%^|*+-/:<=>?@^_~"
@@ -8,10 +9,41 @@ symbol = oneOf "!#$%^|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do char '"' -- Read until we find this char
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest -- : list cons
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          _    -> Atom atom -- _ == .* for pattern matching
+-- XXX liftM
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "scheme" input of
+readExpr input = case parse parseExpr "scheme" input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found val: " ++ show val
+    Right val -> "Found val"
 
 main :: IO ()
 main = do
