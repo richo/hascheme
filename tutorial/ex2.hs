5162e4d3acef26df217e86a47b3897c64007d9d4
Richo Healey <richo@psych0tik.net>
2013-06-05 19:00:27 +1000

    Add whitespace awareness to parser

--- tutorial/ex2.hs
+++ tutorial/ex2.hs
@@ -1,12 +1,15 @@
 module Main where
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
 symbol :: Parser Char
 symbol = oneOf "!#$%^|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "scheme" input of
+readExpr input = case parse (spaces >> symbol) "scheme" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found val: " ++ show val
 
