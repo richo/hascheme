3172939ff5d59eeb2f23699b6cb131a3c1d96506
Richo Healey <richo@psych0tik.net>
2013-06-17 21:08:54 +1000

    Add external representations

---
+++ tutorial/ex3.hs
@@ -0,0 +1,86 @@
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+symbol :: Parser Char
+symbol = oneOf "!#$%^|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do char '"' -- Read until we find this char
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest -- : list cons
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          _    -> Atom atom -- _ == .* for pattern matching
+
+parseNumber :: Parser LispVal
+parseNumber = do digits <- many1 digit
+                 return $ Number (read digits)
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- try parseList <|> parseDottedList
+               char ')'
+               return x
+
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+                  head <- endBy parseExpr spaces
+                  tail <- char '.' >> spaces >> parseExpr
+                  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+              char '\''
+              x <- parseExpr
+              return $ List [Atom "quote", x]
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+showVal :: LispVal -> String
+showVal (String contents)      = "\"" ++ contents ++ "\""
+showVal (Atom name)            = name
+showVal (Number contents)      = show contents
+showVal (Bool True)            = "#t"
+showVal (Bool False)           = "#f"
+showVal (List contents)        = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "scheme" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found val"
+
+main :: IO ()
+main = do
+    line <- getLine
+    putStrLn (readExpr line)
+    main
