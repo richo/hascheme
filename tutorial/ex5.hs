43f3fe583eb67515d876fef7ffc2fb111e2c7de9
Richo Healey <richo@psych0tik.net>
2013-06-18 21:35:14 +1000

    implement cons

--- tutorial/ex5.hs
+++ tutorial/ex5.hs
@@ -149,6 +149,7 @@ primitives = [("+", numericBinop (+)),
               ("||", boolBoolBinop (||)),
               ("car", car),
               ("cdr", cdr),
+              ("cons", cons),
               ("string=?", strBoolBinop (==)),
               ("string<?", strBoolBinop (<)),
               ("string>?", strBoolBinop (>)),
@@ -231,6 +232,14 @@ cdr [DottedList [_] x] = return x
 cdr [DottedList (_ : xs) x] = return $ DottedList xs x
 cdr [badArg] = throwError $ TypeMismatch "pair" badArg
 cdr badArgList = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List []] = return $ List [x1]
+cons [x, List xs] = return $ List $ x : xs
+cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
+cons [x1, x2] = return $ DottedList [x1] x2
+cons badArgList = throwError $ NumArgs 2 badArgList
+
 
 instance Show LispVal where show = showVal
 
