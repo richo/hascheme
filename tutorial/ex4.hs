35091b7a86bbf0a4b2a3ae43808682eaaa9c9c61
Richo Healey <richo@psych0tik.net>
2013-06-18 20:20:50 +1000

    Create ex4 skeleton

---
+++ tutorial/ex4.hs
@@ -0,0 +1,145 @@
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+symbol :: Parser Char
+symbol = oneOf "!#$%^|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do char '"' -- Read until we find this char
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest -- : list cons
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          _    -> Atom atom -- _ == .* for pattern matching
+
+parseNumber :: Parser LispVal
+parseNumber = do digits <- many1 digit
+                 return $ Number (read digits)
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- try parseList <|> parseDottedList
+               char ')'
+               return x
+
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+                  head <- endBy parseExpr spaces
+                  tail <- char '.' >> spaces >> parseExpr
+                  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+              char '\''
+              x <- parseExpr
+              return $ List [Atom "quote", x]
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+showVal :: LispVal -> String
+showVal (String contents)      = "\"" ++ contents ++ "\""
+showVal (Atom name)            = name
+showVal (Number contents)      = show contents
+showVal (Bool True)            = "#t"
+showVal (Bool False)           = "#f"
+showVal (List contents)        = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+eval :: LispVal -> LispVal
+eval val@(String _)             = val
+eval val@(Number _)             = val
+eval val@(Bool _)               = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args))  = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem),
+              ("symbol?", typePredicate isAtom),
+              ("symbol->string", atomToString),
+              ("string->symbol", stringToAtom),
+              ("string?", typePredicate isString),
+              ("number?", typePredicate isNumber)]
+
+typePredicate :: (LispVal -> Bool) -> [LispVal] -> LispVal
+typePredicate predicate params = Bool $ predicate $ head params
+
+isNumber :: LispVal -> Bool
+isNumber (Number _) = True
+isNumber _          = False
+
+isAtom :: LispVal -> Bool
+isAtom (Atom _) = True
+isAtom _        = False
+
+isString :: LispVal -> Bool
+isString (String _) = True
+isString _          = False
+
+atomToString :: [LispVal] -> LispVal
+atomToString params = case head params of
+                    (Atom n) -> String n
+                    _        -> Bool False
+
+stringToAtom :: [LispVal] -> LispVal
+stringToAtom params = case head params of
+                    (String n) -> Atom n
+                    _          -> Bool False
+
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum _ = 0
+
+instance Show LispVal where show = showVal
+
+
+readExpr :: String -> LispVal
+readExpr input = case parse parseExpr "scheme" input of
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
+
+main :: IO ()
+main = do
+    line <- getLine
+    print (eval $ readExpr line)
+    main
